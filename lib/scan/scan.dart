ef1bbf07719d7d598eeb0691d4c62271c6118112
ankitmishra2232 <mca.2125@unigoa.ac.in>
2022-11-14 12:08:38 +0530

    scan by mannual

--- lib/scan/scan.dart
+++ lib/scan/scan.dart
@@ -10,7 +10,8 @@ class scan extends StatefulWidget {
 
 class _scanState extends State<scan> {
   String _data ="";
-
+  late String information;
+  TextEditingController _inputdata = TextEditingController();
   _scan() async{
     return await FlutterBarcodeScanner.scanBarcode("#000000", "Cancel", true, ScanMode.BARCODE)
         .then((value) => setState(()=>_data = value));
@@ -26,42 +27,50 @@ class _scanState extends State<scan> {
           children: [
             ElevatedButton(
                 onPressed:()=> _scan(),
-                child: Text("Scan")),
-            Text(_data)
+                child: Text("Scan")
+            ),
+            TextFormField(
+              keyboardType: TextInputType.phone,
+              controller: _inputdata,
+              decoration: const InputDecoration(
+                icon: const Icon(Icons.location_on),
+                hintText: 'Enter Unique code',
+                labelText: 'Unique ID',
+              ),
+            ),
+            Text(_inputdata.text),
+            Text(_data),
+            Container(
+                // padding: EdgeInsets.only(left: 120.0, top: 40.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                      // formKey.currentState!.save();
+                      print(_data);
+                      print(_inputdata.text);
+                      if (_data == "-1"){
+                        information = _inputdata.text;
+                      }
+                      else{
+                        information = _data;
+                      }
+
+                      print("information  "+ information);
+                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(information)));
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue, //button's fill color
+                    onPrimary: Colors.black, //specify the color of the button's text and icons as well as the overlay colors used to indicate the hover, focus, and pressed states
+                    // onSurface: Colors.black, //specify the button's disabled text, icon, and fill color
+                    shadowColor: Colors.red, //specify the button's elevation color
+                    elevation: 4.0, //buttons Material shadow
+                    textStyle: TextStyle(fontFamily: 'roboto',fontWeight: FontWeight.w800,fontSize: 25), //specify the button's text TextStyle
+                  ),
+                  child: Text('Submit'),
+                )
+            )
           ],
       ),
     );
   }
 }
 
-
-// class scan extends StatefulWidget {
-//   const scan({Key? key}) : super(key: key);
-//   String _data ="";
-//
-//   Future<String> _scan() async{
-//     return await FlutterBarcodeScanner.scanBarcode("#000000", "Cancel", true, ScanMode.BARCODE);
-//   }
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text("Scanning"),
-//       ),
-//         body: Column(
-//           children: [
-//             ElevatedButton(
-//                 onPressed: ()async =>setState(()=>_data = _scan()),
-//                 child: Text("Scan")),
-//             Text(_data)
-//           ],
-//       ),
-//     );
-//   }
-//
-//   @override
-//   State<StatefulWidget> createState() {
-//     // TODO: implement createState
-//     throw UnimplementedError();
-//   }
-// }
